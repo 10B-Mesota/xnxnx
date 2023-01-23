2eeb4b419dc93e28621b8867f646ee8a12c7b2fb
sera <serdafim24@gmail.com>
2023-01-23 11:12:36 -0800

    mesaj

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -10,7 +10,7 @@ const Card = (props: CardProps) => {
           <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
         </figure>
         <div className="card-body">
-          <h2 className="card-title">Shoes!</h2>
+          <h2 className="card-title">{Card.name}</h2>
           <p>If a dog chews shoes whose shoes does he choose?</p>
           <div className="card-actions justify-end">
             <button className="btn-primary btn">Buy Now</button>
