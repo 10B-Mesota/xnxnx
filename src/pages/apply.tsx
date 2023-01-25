932c8fff1b52723ec05463c67cbac49e97b25c85
Sc1pex <scipex.dev@gmail.com>
2023-01-25 17:10:22 +0200

    asd

--- src/pages/apply.tsx
+++ src/pages/apply.tsx
@@ -6,9 +6,13 @@ import { api } from "../utils/api";
 const Apply: NextPage = () => {
   const router = useRouter();
   const { event_name } = router.query;
+  const update_photo = api.event.setEventPhoto.useMutation();
 
   return (
-    <>{event_name ? <>apllying to {event_name}</> : <>Invalid post id</>}</>
+    <>
+      {event_name ? <>apllying to {event_name}</> : <>Invalid post id</>}
+      <form></form>
+    </>
   );
 };
 
